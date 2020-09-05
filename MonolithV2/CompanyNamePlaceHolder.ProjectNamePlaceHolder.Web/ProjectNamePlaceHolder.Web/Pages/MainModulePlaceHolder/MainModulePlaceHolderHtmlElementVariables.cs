be92d87df0cb4176c6881aa162124e1ae2743a89
Samuel N. Amor <samuel.amor@filinvestcity.com>
2020-09-05 10:32:59 +0800

    Working update

--- MonolithV2/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/ProjectNamePlaceHolder.Web/Pages/MainModulePlaceHolder/MainModulePlaceHolderHtmlElementVariables.cs
+++ MonolithV2/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/ProjectNamePlaceHolder.Web/Pages/MainModulePlaceHolder/MainModulePlaceHolderHtmlElementVariables.cs
@@ -6,43 +6,7 @@ using System.Collections.Generic;
 namespace ProjectNamePlaceHolder.Web.Pages.MainModulePlaceHolder
 {
     public static class MainModulePlaceHolderHtmlElementVariables
-    {
-        public const string Handler_GetRecord = "Record";
-
-        public const string FormModal_CreateEdit = "CreateEdit";
-        public const string ConfirmModal_Save = "ConfirmSaveModal";
-        public const string Handler_Create = "Create";
-        public const string Handler_Save = "Save";
-        public const string JSTriggerShow_Create = "Create";
-        public const string Button_FormActionSave = "btnSave";
-        public const string Button_ShowConfirmSaveModal = "btnShowSaveModal";
-  
-  
-
-        public const string FormModal_Edit = "Edit";
-        public const string ConfirmModal_Update = "ConfirmUpdateModal";
-        public const string PageName_Edit = "_Edit";
-        public const string Handler_Update = "Update";
-        public const string JSTriggerShow_Edit = "Edit";
-        public const string Button_FormActionUpdate = "btnUpdate";
-        public const string Button_ShowConfirmUpdateModal = "btnShowUpdateModal";
-        public const string PromptContainer_Edit = "PromptEditContainer";
-        public const string FormName_Edit = "FormEdit";
-
-        public const string FormModal_Delete = "Delete";
-        public const string ConfirmModal_Delete = "ConfirmDeleteModal";
-        public const string PageName_Delete = "_Delete";
-        public const string Handler_Delete = "Delete";
-        public const string JSTriggerShow_Delete = "Delete";
-        public const string Button_FormActionDelete = "btnDelete";
-        public const string Button_ShowConfirmDeleteModal = "btnShowDeleteModal";
-        public const string PromptContainer_Delete = "PromptDeleteContainer";
-        public const string FormName_Delete = "FormDelete";
-
-        public const string FormModal_View = "View";
-        public const string PageName_View = "_View";
-        public const string JSTriggerShow_View = "View";
-
+    {     
         public const string MainModulePlaceHolderForm = "formMainModulePlaceHolderForm";
         public const string MainModulePlaceHolderFormPromptContainer = "promptMainModulePlaceHolderForm";
         public static readonly FormModal MainModulePlaceHolderModal = new FormModal("MainModulePlaceHolderModal", 700);
@@ -51,6 +15,7 @@ namespace ProjectNamePlaceHolder.Web.Pages.MainModulePlaceHolder
         public static readonly PageHandler ShowView = new PageHandler("ShowView", Resource.LabelDetailsMainModulePlaceHolder, new List<string> { "id" });
         public static readonly PageHandler ShowDelete = new PageHandler("ShowDelete", Resource.LabelDeleteMainModulePlaceHolder, new List<string> { "id" });
         public static readonly PageHandler Save = new PageHandler(name: "Save", withPromptConfirmation: true);
+        public static readonly PageHandler Update = new PageHandler(name: "Update", withPromptConfirmation: true);
     }
 }
 
