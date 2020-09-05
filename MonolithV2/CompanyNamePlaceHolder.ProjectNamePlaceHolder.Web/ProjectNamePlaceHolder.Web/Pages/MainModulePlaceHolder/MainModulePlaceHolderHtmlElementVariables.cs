84972556b8859773b276ba1a869e27fa4be2107b
Samuel N. Amor <samuel.amor@filinvestcity.com>
2020-09-05 20:59:56 +0800

    Fix handler

--- MonolithV2/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/ProjectNamePlaceHolder.Web/Pages/MainModulePlaceHolder/MainModulePlaceHolderHtmlElementVariables.cs
+++ MonolithV2/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/ProjectNamePlaceHolder.Web/Pages/MainModulePlaceHolder/MainModulePlaceHolderHtmlElementVariables.cs
@@ -13,14 +13,14 @@ namespace ProjectNamePlaceHolder.Web.Pages.MainModulePlaceHolder
         public const string MainModulePlaceHolderListingsContainer = "containerMainModulePlaceHolderListings";
 
         public static readonly FormModal MainModulePlaceHolderModal = new FormModal("MainModulePlaceHolderModal", 700);
-        public static readonly PageHandler ShowCreate = new PageHandler("ShowCreate", Resource.LabelAddMainModulePlaceHolder);
-        public static readonly PageHandler ShowEdit = new PageHandler("ShowEdit", Resource.LabelEditMainModulePlaceHolder, new List<string> { "id" });
-        public static readonly PageHandler ShowView = new PageHandler("ShowView", Resource.LabelDetailsMainModulePlaceHolder, new List<string> { "id" });
-        public static readonly PageHandler ShowDelete = new PageHandler("ShowDelete", Resource.LabelDeleteMainModulePlaceHolder, new List<string> { "id" });
-        public static readonly PageHandler Save = new PageHandler(name: "Save", withPromptConfirmation: true);
-        public static readonly PageHandler Update = new PageHandler(name: "Update", withPromptConfirmation: true);
-        public static readonly PageHandler Delete = new PageHandler(name: "Delete", withPromptConfirmation: true);
-        public static readonly PageHandler InitializeList = new PageHandler(name: "InitializeList");
+        public static readonly PageHandler ShowCreateHandler = new PageHandler("ShowCreate", Resource.LabelAddMainModulePlaceHolder);
+        public static readonly PageHandler ShowEditHandler = new PageHandler("ShowEdit", Resource.LabelEditMainModulePlaceHolder, new List<string> { "id" });
+        public static readonly PageHandler ShowViewHandler = new PageHandler("ShowView", Resource.LabelDetailsMainModulePlaceHolder, new List<string> { "id" });
+        public static readonly PageHandler ShowDeleteHandler = new PageHandler("ShowDelete", Resource.LabelDeleteMainModulePlaceHolder, new List<string> { "id" });
+        public static readonly PageHandler SaveHandler = new PageHandler(name: "Save", withPromptConfirmation: true);
+        public static readonly PageHandler UpdateHandler = new PageHandler(name: "Update", withPromptConfirmation: true);
+        public static readonly PageHandler DeleteHandler = new PageHandler(name: "Delete", withPromptConfirmation: true);
+        public static readonly PageHandler InitializeListHandler = new PageHandler(name: "InitializeList");
     }
 }
 
