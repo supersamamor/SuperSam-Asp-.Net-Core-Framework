3563bf57d8c9a35791604a3a71d2e0651b3969c8
Samuel N. Amor <samuel.amor@filinvestcity.com>
2020-09-13 10:01:54 +0800

    Refactor page handler

--- MonolithV2/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/ProjectNamePlaceHolder.Web/Extensions/PageHandler.cs
+++ MonolithV2/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/ProjectNamePlaceHolder.Web/Extensions/PageHandler.cs
@@ -7,18 +7,18 @@ namespace ProjectNamePlaceHolder.Web.Extensions
     {
         public PageHandler(string name, bool withPromptConfirmation = false)
         {
-            this.Name = name;  
-            this.WithPromptConfirmation = withPromptConfirmation;   
+            this.Name = name;
+            this.WithPromptConfirmation = withPromptConfirmation;
         }
 
         public PageHandler(string name, bool jqueryValidate, bool withPromptConfirmation = false)
         {
             this.Name = name;
             this.WithPromptConfirmation = withPromptConfirmation;
-            this.JQueryValidate = jqueryValidate;            
+            this.JQueryValidate = jqueryValidate;
         }
 
-        public PageHandler(string name, string description) 
+        public PageHandler(string name, string description)
         {
             this.Name = name;
             this.Description = description;
@@ -87,7 +87,7 @@ namespace ProjectNamePlaceHolder.Web.Extensions
             var htmlstring = @"<script type=""text/javascript"">";
             htmlstring += @"           function " + this.JSFunctionTriggerHandler + @"() {";
             htmlstring += @"                $(""#" + targetElement + @""").append(""" + PageLoader(targetElement + "Loader", true) + @""");";
-            htmlstring += @"                $('#" + targetElement + @"').load('/MainModulePlaceHolder?handler=" + this.Name + @""+ HtmlObjectCreator.CreateRoutesForListingHandler(currentModelValues) + @"', function(){ });";
+            htmlstring += @"                $('#" + targetElement + @"').load('/MainModulePlaceHolder?handler=" + this.Name + @"" + HtmlObjectCreator.CreateRoutesForListingHandler(currentModelValues) + @"', function(){ });";
             htmlstring += @"           };";
             htmlstring += @"</script>";
             return new HtmlString(htmlstring);
@@ -95,25 +95,26 @@ namespace ProjectNamePlaceHolder.Web.Extensions
 
         public IHtmlContent CelerSoftPostTriggerHandlerAjax(FormModal modal, string promptMessageContainer, string formName, string confirmationMessage = null, string runJavascriptOnSuccess = null)
         {
+            var promptModalName = modal.Name + this.Name;
             var postString = @"$(""#" + modal.Body + @""").append(""" + PageLoader(modal.Body + "Loader", true) + @""");$.post('?handler=" + this.Name + @"', $('#" + formName + @"').serialize(), function(data) { $('#" + modal.Body + @"').html(data); " + (runJavascriptOnSuccess ?? "") + @" });";
 
             var validateString = $"var form = $('#" + formName + @"'); if ($(form).valid()) { ";
-            validateString += postString + @"} else { $('#" + promptMessageContainer + @"').html('<div class=""alert alert-danger small alert-dismissible fade show"" role=""alert""><span>Please check for invalid or missing fields.</span></div>'); $('#" + promptMessageContainer + @"').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);}";
-                   
-            var htmlstring = this.WithPromptConfirmation ? PromptConfirmationModal(modal, confirmationMessage, postString) : "";
+            validateString += postString + @"} else { $('#" + promptMessageContainer + @"').html('<div class=""alert alert-danger small alert-dismissible fade show"" role=""alert""><span>Please check for invalid or missing fields.</span></div>'); $('#" + promptMessageContainer + @"').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);document.getElementById('" + modal.Body + @"').scrollTop = 0;}";
+
+            var htmlstring = this.WithPromptConfirmation ? PromptConfirmationModal(promptModalName, modal.ZIndex, confirmationMessage, postString) : "";
             htmlstring += @"      <script type=""text/javascript"">";
             htmlstring += @"           function " + this.JSFunctionTriggerHandler + @"() {";
             if (this.WithPromptConfirmation == true)
             {
                 htmlstring += @"       var form = $('#" + formName + @"');";
                 htmlstring += @"       if ($(form).valid()) { ";
-                htmlstring += @"            ShowHideConfirm" + modal.Name + @"();";  
+                htmlstring += @"            ShowHideConfirm" + promptModalName + @"();";
                 htmlstring += @"       }";
                 htmlstring += @"       else {";
-                htmlstring += @"            $('#" + promptMessageContainer + @"').html('<div class=""alert alert-danger small alert-dismissible fade show"" role=""alert""><span>Please check for invalid or missing fields.</span></div>'); $('#" + promptMessageContainer + @"').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);";
+                htmlstring += @"            $('#" + promptMessageContainer + @"').html('<div class=""alert alert-danger small alert-dismissible fade show"" role=""alert""><span>Please check for invalid or missing fields.</span></div>'); $('#" + promptMessageContainer + @"').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);document.getElementById('" + modal.Body + @"').scrollTop = 0;";
                 htmlstring += @"       }";
             }
-            else 
+            else
             {
                 if (this.JQueryValidate == true)
                 {
@@ -123,26 +124,27 @@ namespace ProjectNamePlaceHolder.Web.Extensions
                 {
                     htmlstring += postString;
                 }
-            }  
+            }
             htmlstring += @"           };";
             htmlstring += @"      </script>";
             return new HtmlString(htmlstring);
         }
         public IHtmlContent CelerSoftPostTriggerHandler(FormModal modal, string promptMessageContainer, string formName, string confirmationMessage = null, string runJavascriptOnSuccess = null)
         {
-            var postString = @"document.getElementById('"+ formName + @"').submit();" + (runJavascriptOnSuccess ?? "") + @";";
+            var promptModalName = modal.Name + this.Name;
+            var postString = @"document.getElementById('" + formName + @"').submit();" + (runJavascriptOnSuccess ?? "") + @";";
 
             var validateString = $"var form = $('#" + formName + @"'); if ($(form).valid()) { ";
             validateString += postString + @"} else { $('#" + promptMessageContainer + @"').html('<div class=""alert alert-danger small alert-dismissible fade show"" role=""alert""><span>Please check for invalid or missing fields.</span></div>'); }";
 
-            var htmlstring = this.WithPromptConfirmation ? PromptConfirmationModal(modal, confirmationMessage, postString) : "";
+            var htmlstring = this.WithPromptConfirmation ? PromptConfirmationModal(promptModalName, modal.ZIndex, confirmationMessage, postString) : "";
             htmlstring += @"      <script type=""text/javascript"">";
             htmlstring += @"           function " + this.JSFunctionTriggerHandler + @"() {";
             if (this.WithPromptConfirmation == true)
             {
                 htmlstring += @"       var form = $('#" + formName + @"');";
                 htmlstring += @"       if ($(form).valid()) { ";
-                htmlstring += @"            ShowHideConfirm" + modal.Name + @"();";
+                htmlstring += @"            ShowHideConfirm" + promptModalName + @"();";
                 htmlstring += @"       }";
                 htmlstring += @"       else {";
                 htmlstring += @"            $('#" + promptMessageContainer + @"').html('<div class=""alert alert-danger small alert-dismissible fade show"" role=""alert""><span>Please check for invalid or missing fields.</span></div>'); ";
@@ -164,41 +166,42 @@ namespace ProjectNamePlaceHolder.Web.Extensions
             return new HtmlString(htmlstring);
         }
 
-        private string PromptConfirmationModal(FormModal modal, string confirmationMessage, string postString)
+        private string PromptConfirmationModal(string promptModalName, int modalZIndex, string confirmationMessage, string postString)
         {
-            int initialZindex = modal.ZIndex + 1;           
+
+            int initialZindex = modalZIndex + 1;
             var htmlstring = @"";
             htmlstring += @"      <script type=""text/javascript"">";
-            htmlstring += @"           function ShowHideConfirm" + modal.Name + @"() {";
-            htmlstring += @"                $(""#" + modal.Name + @"PromptModal"").slideToggle(""fast"");";
-            htmlstring += @"                $(""#" + modal.Name + @"PromptModalBackGround"").slideToggle(""fast"");";
+            htmlstring += @"           function ShowHideConfirm" + promptModalName + @"() {";
+            htmlstring += @"                $(""#" + promptModalName + @""").slideToggle(""fast"");";
+            htmlstring += @"                $(""#" + promptModalName + @"BackGround"").slideToggle(""fast"");";
             htmlstring += @"           }";
-            htmlstring += @"           function TriggerConfirm" + modal.Name + @"() {";
+            htmlstring += @"           function TriggerConfirm" + promptModalName + @"() {";
             htmlstring += @"               " + postString;
             htmlstring += @"           }";
             htmlstring += @"      </script>";
-            htmlstring += @"      <div class=""modal"" id=""" + modal.PromptModalNamel + @""" style=""z-index: " + (initialZindex + 1) + @";position:fixed;top:20%;display:none;"">";
+            htmlstring += @"      <div class=""modal"" id=""" + promptModalName + @""" style=""z-index: " + (initialZindex + 1) + @";position:fixed;top:20%;display:none;"">";
             htmlstring += @"           <div class=""modal-dialog"">";
             htmlstring += @"                <div class=""modal-content""  style=""z-index: " + (initialZindex + 1) + @";"">";
             htmlstring += @"                     <div class=""modal-header"">";
             htmlstring += @"                          <h6 class=""modal-title"" style=""font-weight:400;"">Confirmation</h6>";
-            htmlstring += @"                          <button type=""button"" class=""close"" onclick=""ShowHideConfirm" + modal.Name + @"();"">&times;</button>";
+            htmlstring += @"                          <button type=""button"" class=""close"" onclick=""ShowHideConfirm" + promptModalName + @"();"">&times;</button>";
             htmlstring += @"                     </div>";
             htmlstring += @"                     <div class=""modal-body"">";
             htmlstring += @"                          " + confirmationMessage + @"   ";
             htmlstring += @"                     </div>";
             htmlstring += @"                     <div class=""modal-footer"">";
-            htmlstring += @"                          <button type=""button"" class=""btn btn-info"" data-toggle=""tooltip"" data-placement=""top"" title=""Ok"" onclick=""TriggerConfirm" + modal.Name + @"();"">";     
+            htmlstring += @"                          <button type=""button"" class=""btn btn-info"" data-toggle=""tooltip"" data-placement=""top"" title=""Ok"" onclick=""TriggerConfirm" + promptModalName + @"();"">";
             htmlstring += @"                               <i class=""fas fa-check""></i>";
             htmlstring += @"                          </button>";
-            htmlstring += @"                          <button type=""button"" class=""btn btn-danger"" data-placement=""top"" title=""Close"" onclick=""ShowHideConfirm" + modal.Name + @"();"">";
+            htmlstring += @"                          <button type=""button"" class=""btn btn-danger"" data-placement=""top"" title=""Close"" onclick=""ShowHideConfirm" + promptModalName + @"();"">";
             htmlstring += @"                               <i class=""fas fa-times-circle""></i>";
             htmlstring += @"                          </button>";
             htmlstring += @"                     </div>";
             htmlstring += @"                </div>";
             htmlstring += @"           </div>";
             htmlstring += @"      </div>";
-            htmlstring += @"      <div  id=""" + modal.Name + @"PromptModalBackGround"" style=""display:none;position:fixed;top:0;left:0;z-index:" + initialZindex + @";width:100vw;height:100vh;background-color:#000;opacity:0.3;""></div>";
+            htmlstring += @"      <div  id=""" + promptModalName + @"BackGround"" style=""display:none;position:fixed;top:0;left:0;z-index:" + initialZindex + @";width:100vw;height:100vh;background-color:#000;opacity:0.3;""></div>";
             return htmlstring;
         }
     }
