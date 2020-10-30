15279bb64799610eeb2093a0427d845d67a48269
Samuel N. Amor <samuel.amor@filinvestcity.com>
2020-10-30 18:46:30 +0800

    Upload windows service template

---
+++ WindowsServiceTemplate/Installing a Service.bat
@@ -0,0 +1,10 @@
+REM "C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil.exe" "c:\ProjectNamePlaceHolder.exe"
+cd ..
+
+cd ..
+
+cd "C:\Users\Lenovo PC 251\Desktop\SampleService\CompanyNamePlaceHolder.ProjectNamePlaceHolder\ProjectNamePlaceHolder\bin\Debug"
+
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil.exe" "ProjectNamePlaceHolder.exe"
+
+pause
