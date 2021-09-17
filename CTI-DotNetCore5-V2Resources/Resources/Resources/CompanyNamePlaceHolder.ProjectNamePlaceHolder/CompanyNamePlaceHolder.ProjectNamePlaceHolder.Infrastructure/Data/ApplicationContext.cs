8a15c6846dc7c4eb813f1b56f34fbbb2b7b18d8d
Samuel N. Amor IV <supersam.amor@gmail.com>
2021-09-17 08:41:43 +0800

    Update Template

--- CTI-DotNetCore5-V2Resources/Resources/Resources/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Infrastructure/Data/ApplicationContext.cs
+++ CTI-DotNetCore5-V2Resources/Resources/Resources/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Infrastructure/Data/ApplicationContext.cs
@@ -16,8 +16,8 @@ namespace CompanyNamePlaceHolder.ProjectNamePlaceHolder.Infrastructure.Data
             _authenticatedUser = authenticatedUser;
         }
 
-        public DbSet<MainModulePlaceHolder> MainModulePlaceHolder { get; set; } = default!;
-
+		Template:[InsertNewDataModelContextPropertyTextHere]
+		
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             foreach (var property in modelBuilder.Model.GetEntityTypes()
@@ -26,11 +26,12 @@ namespace CompanyNamePlaceHolder.ProjectNamePlaceHolder.Infrastructure.Data
                                                                    || p.ClrType == typeof(decimal?)))
             {
                 property.SetColumnType("decimal(18,6)");
-            }   
-            modelBuilder.Entity<MainModulePlaceHolder>().HasIndex(e => e.LastModifiedDate);
-            modelBuilder.Entity<MainModulePlaceHolder>().HasIndex(e => e.Entity);
-            modelBuilder.Entity<MainModulePlaceHolder>().HasQueryFilter(e => e.Entity == _authenticatedUser.Entity);
-
+            }    
+			
+			Template:[InsertNewEFFluentAttributesTextHere]
+			Template:[InsertNewEFFluentAttributesUniqueTextHere]
+			Template:[InsertNewEFFluentAttributesStringLengthTextHere]			
+			
             base.OnModelCreating(modelBuilder);
         }
     }
