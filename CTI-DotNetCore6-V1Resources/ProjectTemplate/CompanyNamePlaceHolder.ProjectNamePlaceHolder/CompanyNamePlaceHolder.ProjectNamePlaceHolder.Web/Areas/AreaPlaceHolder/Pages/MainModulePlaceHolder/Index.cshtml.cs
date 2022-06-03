3c747fadfeb438568e11188ebd2ba73835c06918
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-06-03 15:27:42 +0800

    update project

--- CTI-DotNetCore6-V1Resources/ProjectTemplate/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/Areas/AreaPlaceHolder/Pages/MainModulePlaceHolder/Index.cshtml.cs
+++ CTI-DotNetCore6-V1Resources/ProjectTemplate/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/Areas/AreaPlaceHolder/Pages/MainModulePlaceHolder/Index.cshtml.cs
@@ -37,7 +37,7 @@ public class IndexModel : BasePageModel<IndexModel>
 	
 	public async Task<IActionResult> OnGetSelect2Data([FromQuery] Select2Request request)
     {
-        var result = await Mediatr.Send(request.ToQuery<GetMainModulePlaceHolderQuery>(nameof(MainModulePlaceHolderState.Name)));
-        return new JsonResult(result.ToSelect2Response(e => new Select2Result { Id = e.Id, Text = e.Name }));
+        var result = await Mediatr.Send(request.ToQuery<GetMainModulePlaceHolderQuery>(nameof(MainModulePlaceHolderState.Template:[InsertNewFieldName])));
+        return new JsonResult(result.ToSelect2Response(e => new Select2Result { Id = e.Id, Text = e.Template:[InsertNewFieldName] }));
     }
 }
