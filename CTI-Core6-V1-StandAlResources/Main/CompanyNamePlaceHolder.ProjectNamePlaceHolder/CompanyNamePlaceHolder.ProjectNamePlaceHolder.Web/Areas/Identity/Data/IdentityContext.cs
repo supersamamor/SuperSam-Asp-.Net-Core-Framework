95aa5589e45e6f3157d03a73470b0e0aa07c98cc
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-07-14 17:53:51 +0800

    update

--- CTI-Core6-V1-StandAlResources/Main/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/Areas/Identity/Data/IdentityContext.cs
+++ CTI-Core6-V1-StandAlResources/Main/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/Areas/Identity/Data/IdentityContext.cs
@@ -1,3 +1,4 @@
+using CompanyNamePlaceHolder.ProjectNamePlaceHolder.Core.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
--- CTI-DotNetCore6-V1Original/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Core/Identity/ApplicationUser.cs
+++ CTI-DotNetCore6-V1Original/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Core/Identity/ApplicationUser.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 
-namespace CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web.Areas.Identity.Data;
+namespace CompanyNamePlaceHolder.ProjectNamePlaceHolder.Core.Identity;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
