1fd71957596ee76380fd74f897f39f4d8c31ba98
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-07-18 17:07:37 +0800

    update

---
+++ CTI-Core6-V1-StandAlResources/Misc/ApprovalProjectTemplate/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/Areas/AreaPlaceHolder/Pages/MainModulePlaceHolder/Approve.cshtml.cs
@@ -0,0 +1,52 @@
+using CompanyNamePlaceHolder.ProjectNamePlaceHolder.Application.Features.AreaPlaceHolder.Approval.Commands;
+using CompanyNamePlaceHolder.ProjectNamePlaceHolder.Application.Features.AreaPlaceHolder.Approval.Queries;
+using CompanyNamePlaceHolder.ProjectNamePlaceHolder.Application.Features.AreaPlaceHolder.MainModulePlaceHolder.Queries;
+using CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web.Areas.AreaPlaceHolder.Models;
+using CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web.Areas.AreaPlaceHolder.Pages.MainModulePlaceHolder;
+
+[Authorize(Policy = Permission.MainModulePlaceHolder.Approve)]
+public class ApproveModel : BasePageModel<ApproveModel>
+{
+    [BindProperty]
+    public MainModulePlaceHolderViewModel MainModulePlaceHolder { get; set; } = new();
+    [BindProperty]
+    public string? ApprovalStatus { get; set; }
+    public async Task<IActionResult> OnGet(string? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        _ = (await Mediatr.Send(new GetApprovalStatusPerApproverByIdQuery(id))).Select(l => ApprovalStatus = l);
+        return await PageFrom(async () => await Mediatr.Send(new GetMainModulePlaceHolderByIdQuery(id)), MainModulePlaceHolder);
+    }
+
+    public async Task<IActionResult> OnPost(string handler)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        if (handler == "Approve")
+        {
+            return await Approve();
+        }
+        else if (handler == "Reject")
+        {
+            return await Reject();
+        }
+        return Page();
+    }
+    private async Task<IActionResult> Approve()
+    {
+        return await TryThenRedirectToPage(async () => await Mediatr.Send(new ApproveCommand(MainModulePlaceHolder.Id, "")), "Approve", true);
+    }
+    private async Task<IActionResult> Reject()
+    {
+        return await TryThenRedirectToPage(async () => await Mediatr.Send(new RejectCommand(MainModulePlaceHolder.Id, "")), "Approve", true);
+    }
+}
