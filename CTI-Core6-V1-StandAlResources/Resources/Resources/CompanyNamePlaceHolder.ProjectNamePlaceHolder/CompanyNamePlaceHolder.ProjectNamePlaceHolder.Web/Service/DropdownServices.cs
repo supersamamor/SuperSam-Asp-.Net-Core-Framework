a05d67f3582d3b762ff7594e1992f003af981f4a
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-07-25 17:34:33 +0800

    update

--- CTI-Core6-V1-StandAlResources/Resources/Resources/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/Service/DropdownServices.cs
+++ CTI-Core6-V1-StandAlResources/Resources/Resources/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/Service/DropdownServices.cs
@@ -2,16 +2,21 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using CompanyNamePlaceHolder.ProjectNamePlaceHolder.Infrastructure.Data;
 using CompanyNamePlaceHolder.ProjectNamePlaceHolder.Core.AreaPlaceHolder;
 using CompanyNamePlaceHolder.Common.Data;
+using CTI.TenantSales.Web.Areas.Admin.Queries.Users;
+using MediatR;
 
 namespace CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web.Service
 {
     public class DropdownServices
     {
-        private readonly ApplicationContext _context;
-        public DropdownServices( ApplicationContext context)
-        {            
-            _context = context;
-        }     
+		private readonly ApplicationContext _context;
+		private readonly IMediator _mediaTr;
+
+		public DropdownServices(ApplicationContext context, IMediator mediaTr)
+		{            
+			_context = context;
+			_mediaTr = mediaTr;
+		}       
 		Template:[InsertNewGetDropdownMethodFromDropdownService]
 		Template:[ApprovalUserListDropdown]
     }
