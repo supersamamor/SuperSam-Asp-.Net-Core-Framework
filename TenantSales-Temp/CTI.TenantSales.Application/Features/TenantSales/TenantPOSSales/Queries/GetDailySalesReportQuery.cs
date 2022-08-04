0218630c81eb343cb99f442cc2efa586d4f73442
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-08-04 15:55:44 +0800

    update

---
+++ TenantSales-Temp/CTI.TenantSales.Application/Features/TenantSales/TenantPOSSales/Queries/GetDailySalesReportQuery.cs
@@ -0,0 +1,47 @@
+using CTI.TenantSales.Core.TenantSales;
+using CTI.TenantSales.Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CTI.TenantSales.Application.Features.TenantSales.TenantPOSSales.Queries;
+
+public record GetDailySalesReportQuery(DateTime DateFrom, DateTime DateTo, string? TenantId, string? LevelId, string? ProjectId) : IRequest<IList<TenantPOSSalesState>>;
+public class GetDailySalesReportQueryHandler : IRequestHandler<GetDailySalesReportQuery, IList<TenantPOSSalesState>>
+{
+    private readonly ApplicationContext _context;
+    public GetDailySalesReportQueryHandler(ApplicationContext context)
+    {
+        _context = context;
+    }
+    public async Task<IList<TenantPOSSalesState>> Handle(GetDailySalesReportQuery request, CancellationToken cancellationToken = default)
+    {
+        var query = _context.TenantPOSSales
+            .Include(l => l.TenantPOS).ThenInclude(l => l!.Tenant).ThenInclude(l => l!.Project).AsNoTracking();
+        if (request.DateFrom != Convert.ToDateTime(DateTime.MinValue))
+        {
+            query = query.Where(l => l.SalesDate >= request.DateFrom);
+        }
+
+        if (request.DateTo != Convert.ToDateTime(DateTime.MinValue))
+        {
+            query = query.Where(l => l.SalesDate <= request.DateTo);
+        }
+        if (!string.IsNullOrEmpty(request.TenantId))
+        {
+            query = query.Where(l => l.TenantPOS!.TenantId == request.TenantId);
+        }
+        else if (!string.IsNullOrEmpty(request.LevelId))
+        {
+            query = query.Where(l => l.TenantPOS!.Tenant!.LevelId == request.LevelId);
+        }
+        else if (!string.IsNullOrEmpty(request.ProjectId))
+        {
+            query = query.Where(l => l.TenantPOS!.Tenant!.ProjectId == request.ProjectId);
+        }
+        return await query.OrderBy(l => l.TenantPOS!.Tenant!.Project!.Name)
+            .ThenBy(l => l.TenantPOS!.Tenant!.Name)
+             .ThenBy(l => l.TenantPOS!.Code)
+             .ThenBy(l => l.SalesCategory)
+             .ThenBy(l => l.SalesDate).ToListAsync();
+    }
+}
--- TenantSales-Temp/CTI.TenantSales.Web/Areas/Reports/Pages/DailySales/Index.cshtml.cs
+++ TenantSales-Temp/CTI.TenantSales.Web/Areas/Reports/Pages/DailySales/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using CTI.TenantSales.Application.Features.TenantSales.TenantPOSSales.Queries;
 using CTI.TenantSales.ExcelProcessor.Services;
 using CTI.TenantSales.Web.Areas.Reports.Models;
 using CTI.TenantSales.Web.Models;
@@ -10,34 +11,35 @@ namespace CTI.TenantSales.Web.Areas.Reports.Pages.DailySales
     [Authorize(Policy = Permission.Reports.DailySales)]
     public class IndexModel : BasePageModel<IndexModel>
     {
-		private readonly ExportDailySalesReportService _exportDailySalesReportService;
-		private readonly int _cutOffFrom;
-		private readonly int _cutOffTo;
-		public IndexModel(ExportDailySalesReportService exportDailySalesReportService, IConfiguration configuration)
-		{
-			_exportDailySalesReportService = exportDailySalesReportService;
-			_cutOffFrom = configuration.GetValue<int>("CutOff:From");
-			_cutOffTo = configuration.GetValue<int>("CutOff:To");
-		}
-		[BindProperty]
+        private readonly ExportDailySalesReportService _exportDailySalesReportService;
+        private readonly int _cutOffFrom;
+        private readonly int _cutOffTo;
+        public IndexModel(ExportDailySalesReportService exportDailySalesReportService, IConfiguration configuration)
+        {
+            _exportDailySalesReportService = exportDailySalesReportService;
+            _cutOffFrom = configuration.GetValue<int>("CutOff:From");
+            _cutOffTo = configuration.GetValue<int>("CutOff:To");
+        }
+        [BindProperty]
         public DailySalesModel Input { get; set; } = new();
-		public IActionResult OnGet()
+        public IActionResult OnGet()
         {
             return Page();
         }
-		public IActionResult OnPost()
-		{
-			ModelState.Clear();
-			var reportDate = new DateTime(Input.Year, Input.Month + 1, 1);
-			DateTime dateFrom = new DateTime(reportDate.AddMonths(-1).Year, reportDate.AddMonths(-1).Month, _cutOffFrom);
-			DateTime dateTo = new DateTime(reportDate.Year, reportDate.Month, _cutOffTo);
-			_exportDailySalesReportService.Export(dateFrom, dateTo, null);
-			return Partial("_InputFieldsPartial", Input);
-		}
-		public IActionResult OnPostChangeFormValue()
-		{
-			ModelState.Clear();
-			return Partial("_InputFieldsPartial", Input);
-		}
-	}
+        public async Task<IActionResult> OnPost()
+        {
+            ModelState.Clear();
+            var reportDate = new DateTime(Input.Year, Input.Month + 1, 1);
+            DateTime dateFrom = new(reportDate.AddMonths(-1).Year, reportDate.AddMonths(-1).Month, _cutOffFrom);
+            DateTime dateTo = new(reportDate.Year, reportDate.Month, _cutOffTo);
+            var tenantPOSList = await Mediatr.Send(new GetDailySalesReportQuery(dateFrom, dateTo, Input.TenantId, Input.LevelId, Input.ProjectId));
+            Input.FilePath = _exportDailySalesReportService.Export(dateFrom, dateTo, tenantPOSList);
+            return Partial("_InputFieldsPartial", Input);
+        }
+        public IActionResult OnPostChangeFormValue()
+        {
+            ModelState.Clear();
+            return Partial("_InputFieldsPartial", Input);
+        }
+    }
 }
