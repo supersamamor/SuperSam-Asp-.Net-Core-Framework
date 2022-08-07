ad0651e2f4b96d7a4dcf794f1cd14996d03f7744
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-08-07 13:13:36 +0800

    update

--- TenantSales-Temp/CTI.TenantSales.Core/TenantSales/CategoryState.cs
+++ TenantSales-Temp/CTI.TenantSales.Core/TenantSales/CategoryState.cs
@@ -7,9 +7,7 @@ public record CategoryState : BaseEntity
 {
 	public string Name { get; init; } = "";
 	public string ClassificationId { get; init; } = "";
-	public string Code { get; init; } = "";
-	
+	public string Code { get; init; } = "";	
 	public ClassificationState? Classification { get; init; }
-	
-	
+	public IList<TenantState>? TenantList { get; init; }
 }
