bbdf6330b74ed5fb96043d8f6a1b4d0e6dcbcfc8
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-08-08 08:58:49 +0800

    update

--- TenantSales-Temp/CTI.TenantSales.Application/Features/TenantSales/Revalidate/Commands/AddRevalidateCommand.cs
+++ TenantSales-Temp/CTI.TenantSales.Application/Features/TenantSales/Revalidate/Commands/AddRevalidateCommand.cs
@@ -23,13 +23,13 @@ public class AddRevalidateCommandHandler : BaseCommandHandler<ApplicationContext
     {
     }
 
-    
-public async Task<Validation<Error, RevalidateState>> Handle(AddRevalidateCommand request, CancellationToken cancellationToken) =>
-		await Validators.ValidateTAsync(request, cancellationToken).BindT(
-			async request => await Add(request, cancellationToken));
-	
-	
-	
+
+    public async Task<Validation<Error, RevalidateState>> Handle(AddRevalidateCommand request, CancellationToken cancellationToken) =>
+            await Validators.ValidateTAsync(request, cancellationToken).BindT(
+                async request => await Add(request, cancellationToken));
+
+
+
 }
 
 public class AddRevalidateCommandValidator : AbstractValidator<AddRevalidateCommand>
@@ -43,7 +43,12 @@ public class AddRevalidateCommandValidator : AbstractValidator<AddRevalidateComm
         RuleFor(x => x.Id).MustAsync(async (id, cancellation) => await _context.NotExists<RevalidateState>(x => x.Id == id, cancellationToken: cancellation))
                           .WithMessage("Revalidate with id {PropertyValue} already exists");
 
-        RuleFor(x => new { x.ProjectId, x.TenantId, x.SalesDate }).MustAsync(async (a, cancellation) => await _context.NotExists<RevalidateState>(x => x.ProjectId == a.ProjectId && x.TenantId == a.TenantId && x.SalesDate == a.SalesDate && x.Status == Core.TenantSales.Status.Pending, cancellationToken: cancellation))
+        RuleFor(x => new { x.ProjectId, x.TenantId, x.SalesDate })
+            .MustAsync(async (a, cancellation) => await _context.NotExists<RevalidateState>(
+                x => x.ProjectId == a.ProjectId
+                && ((string.IsNullOrEmpty(a.TenantId)) || (!string.IsNullOrEmpty(a.TenantId) && x.TenantId == a.TenantId))
+                && x.SalesDate == a.SalesDate
+                && x.Status == Core.TenantSales.Status.Pending, cancellationToken: cancellation))
                   .WithMessage(a => $"This project, tenant and sales date combination is already queued for revalidation.");
     }
 }
