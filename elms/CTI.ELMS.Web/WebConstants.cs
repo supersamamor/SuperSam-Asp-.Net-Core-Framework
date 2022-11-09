9cebe5dede05311fcd6d89f0bb424d67ee4db429
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-11-09 15:46:10 +0800

    update

--- elms/CTI.ELMS.Web/WebConstants.cs
+++ elms/CTI.ELMS.Web/WebConstants.cs
@@ -4,5 +4,7 @@ namespace CTI.ELMS.Web
     {
         public const string UploadFilesPath = "UploadFilesPath";
         public const string DefaultLeadMailSubject = "eLMS";
+        public const string MobileNoMask = "(+99) (999) (999-9999)";
+        public const string MobileNoMaskDefaultValue = "(+63) (   ) (   -    )";
     }
 }
