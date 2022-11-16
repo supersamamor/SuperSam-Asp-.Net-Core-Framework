6cc55e0b6b1bfc966cf772b9b9334fee63a2cc25
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-11-16 16:03:40 +0800

    update

--- elms/CTI.ELMS.Application/Features/ELMS/Project/Queries/GetProjectQuery.cs
+++ elms/CTI.ELMS.Application/Features/ELMS/Project/Queries/GetProjectQuery.cs
@@ -5,6 +5,7 @@ using CTI.ELMS.Infrastructure.Data;
 using MediatR;
 using CTI.Common.Utility.Extensions;
 using Microsoft.EntityFrameworkCore;
+using CTI.Common.Identity.Abstractions;
 
 namespace CTI.ELMS.Application.Features.ELMS.Project.Queries;
 
@@ -12,13 +13,27 @@ public record GetProjectQuery : BaseQuery, IRequest<PagedListResponse<ProjectSta
 
 public class GetProjectQueryHandler : BaseQueryHandler<ApplicationContext, ProjectState, GetProjectQuery>, IRequestHandler<GetProjectQuery, PagedListResponse<ProjectState>>
 {
-    public GetProjectQueryHandler(ApplicationContext context) : base(context)
+    private readonly IAuthenticatedUser _authenticatedUser;
+    public GetProjectQueryHandler(ApplicationContext context, IAuthenticatedUser authenticatedUser) : base(context)
     {
+        _authenticatedUser = authenticatedUser;
     }
-    public override async Task<PagedListResponse<ProjectState>> Handle(GetProjectQuery request, CancellationToken cancellationToken = default) =>
-        await Context.Set<ProjectState>().Include(l => l.EntityGroup).ThenInclude(l => l!.PPlusConnectionSetup)
-        .AsNoTracking().ToPagedResponse(request.SearchColumns, request.SearchValue,
-            request.SortColumn, request.SortOrder,
-            request.PageNumber, request.PageSize,
-            cancellationToken);
+    public override async Task<PagedListResponse<ProjectState>> Handle(GetProjectQuery request, CancellationToken cancellationToken = default)
+    {
+        var query = (from project in Context.Project
+                     select project).AsNoTracking(); 
+        if (_authenticatedUser.ClaimsPrincipal != null && !_authenticatedUser.ClaimsPrincipal.IsInRole(Core.Constants.Roles.Admin))
+        {
+            query = from a in query       
+                    join ap in Context.UserProjectAssignment on a.Id equals ap.ProjectID
+                    where ap.UserId == _authenticatedUser.UserId
+                    select a;
+        }
+        return await query.Include(l => l.EntityGroup).ThenInclude(l => l!.PPlusConnectionSetup)
+                           .ToPagedResponse(request.SearchColumns, request.SearchValue,
+                           request.SortColumn, request.SortOrder,
+                           request.PageNumber, request.PageSize,
+                           cancellationToken);
+    }
+
 }
