703e17267dffc738dc5d105793e0fa30b0f2c3f2
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-11-16 11:02:32 +0800

    update

---
+++ elms/CTI.ELMS.Web/Areas/ELMS/Pages/Lead/EditOffering.cshtml.cs
@@ -0,0 +1,79 @@
+using CTI.ELMS.Application.Features.ELMS.Offering.Commands;
+using CTI.ELMS.Application.Features.ELMS.Offering.Queries;
+using CTI.ELMS.Application.Features.ELMS.TabNavigation.Queries;
+using CTI.ELMS.Application.Features.ELMS.Unit.Queries;
+using CTI.ELMS.Web.Areas.ELMS.Models;
+using CTI.ELMS.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTI.ELMS.Web.Areas.ELMS.Pages.Lead;
+
+[Authorize(Policy = Permission.Offering.Create)]
+public class EditOfferingModel : BasePageModel<EditOfferingModel>
+{
+    [BindProperty]
+    public OfferingViewModel Offering { get; set; } = new();
+    [BindProperty]
+    public string? RemoveSubDetailId { get; set; }
+    [BindProperty]
+    public string? AsyncAction { get; set; }
+    [BindProperty]
+    public string? AddPreSelectedUnitUnitId { get; set; }
+    public LeadTabNavigationPartial LeadTabNavigation { get; set; } = new();
+    public async Task<IActionResult> OnGet(string id)
+    {
+        LeadTabNavigation = Mapper.Map<LeadTabNavigationPartial>(await Mediatr.Send(new GetTabNavigationByOfferingIdQuery(id, Constants.TabNavigation.Offerings)));       
+        return await PageFrom(async () => await Mediatr.Send(new GetOfferingByIdQuery(id)), Offering);
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        LeadTabNavigation = Mapper.Map<LeadTabNavigationPartial>(await Mediatr.Send(new GetTabNavigationByLeadIdQuery(Offering.LeadID!, Constants.TabNavigation.Offerings)));
+        return await TryThenRedirectToPage(async () => await Mediatr.Send(Mapper.Map<AddOfferingCommand>(Offering)), "EditOffering", true);
+    }
+    public async Task<IActionResult> OnPostChangeFormValue()
+    {
+        ModelState.Clear();
+        if (AsyncAction == "RemovePreSelectedUnit")
+        {
+            return RemovePreSelectedUnit();
+        }
+        if (AsyncAction == "AddPreSelectedUnit")
+        {
+            return await AddPreSelectedUnit();
+        }
+        if (AsyncAction == "ChangeProject")
+        {
+            return ChangeProject();
+        }
+        return Partial("_OfferingInputFieldsPartial", Offering);
+    }
+    private IActionResult RemovePreSelectedUnit()
+    {
+        ModelState.Clear();
+        Offering.PreSelectedUnitList = Offering!.PreSelectedUnitList!.Where(l => l.Id != RemoveSubDetailId).ToList();
+        return Partial("_OfferingInputFieldsPartial", Offering);
+    }
+    private async Task<IActionResult> AddPreSelectedUnit()
+    {
+        ModelState.Clear();
+        PreSelectedUnitViewModel preSelectedUnitToAdd = new();
+        _ = (await Mediatr.Send(new GetUnitByIdQuery(AddPreSelectedUnitUnitId!))).Select(l => preSelectedUnitToAdd = Mapper.Map<PreSelectedUnitViewModel>(l));
+        preSelectedUnitToAdd.OfferingID = Offering.Id;
+        if (Offering!.PreSelectedUnitList == null) { Offering!.PreSelectedUnitList = new List<PreSelectedUnitViewModel>(); }
+        Offering!.PreSelectedUnitList!.Add(preSelectedUnitToAdd);
+        return Partial("_OfferingInputFieldsPartial", Offering);
+    }
+    private IActionResult ChangeProject()
+    {
+        ModelState.Clear();
+        Offering.UnitOfferedList = new List<UnitOfferedViewModel>();
+        Offering.PreSelectedUnitList = new List<PreSelectedUnitViewModel>();
+        return Partial("_OfferingInputFieldsPartial", Offering);
+    }
+}
