1156fe4c91b550915841d0d2552039a201787b0a
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-11-17 17:29:27 +0800

    update

---
+++ social_media_login/lib/myapp.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            title: const Text('GeeksforGeeks'),
+          ),
+        ));
+  }
+}
+
+class NewScreen extends StatefulWidget {
+  const NewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NewScreen> createState() => _NewScreenState();
+}
+
+class _NewScreenState extends State<NewScreen> {
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GeeksforGeeks '),
+        backgroundColor: Colors.green,
+      ),
+      body: const Center(
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              'User Interface Changed!!',
+            )),
+      ),
+    );
+  }
+}
