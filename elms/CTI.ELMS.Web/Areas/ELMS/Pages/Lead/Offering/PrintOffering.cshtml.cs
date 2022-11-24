0b5587d34983a1acb58ce5d860f0d017d6e546a5
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-11-24 15:46:41 +0800

    Update

---
+++ elms/CTI.ELMS.Web/Areas/ELMS/Pages/Lead/Offering/PrintOffering.cshtml.cs
@@ -0,0 +1,21 @@
+using CTI.ELMS.Application.Features.ELMS.Offering.Queries;
+using CTI.ELMS.Application.Features.ELMS.TabNavigation.Queries;
+using CTI.ELMS.Web.Areas.ELMS.Models;
+using CTI.ELMS.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTI.ELMS.Web.Areas.ELMS.Pages.Lead.Offering;
+
+[Authorize(Policy = Permission.Offering.Print)]
+public class PrintOfferingModel : BasePageModel<PrintOfferingModel>
+{
+    [BindProperty]
+    public OfferingViewModel Offering { get; set; } = new();
+    public LeadTabNavigationPartial LeadTabNavigation { get; set; } = new();
+    public async Task<IActionResult> OnGet(string id)
+    {
+        LeadTabNavigation = Mapper.Map<LeadTabNavigationPartial>(await Mediatr.Send(new GetTabNavigationByOfferingIdQuery(id, Constants.TabNavigation.Offerings)));
+        return await PageFrom(async () => await Mediatr.Send(new GetOfferingByIdQuery(id)), Offering);
+    }    
+}
