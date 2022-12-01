da3d4115ab318c57fff6925fbc4f0855881ea3e6
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-12-01 18:08:26 +0800

    update

--- SuperSam.InstallerCreator/Installer.cs
+++ SuperSam.InstallerCreator/Installer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.IO;
 using System.Windows.Forms;
 
 namespace SuperSam.InstallerCreator
@@ -14,7 +15,6 @@ namespace SuperSam.InstallerCreator
 
         private void Installer_Load_1(object sender, EventArgs e)
         {
-            string applicationPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
             this.Text = $"Install {ConfigurationManager.AppSettings["Title"]}";
             txtClientId.Text = ConfigurationManager.AppSettings["DefaultClientId"];
             txtClientSecret.Text = ConfigurationManager.AppSettings["DefaultClientSecret"];
@@ -30,8 +30,30 @@ namespace SuperSam.InstallerCreator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-           
+            try
+            {
+                string applicationPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                string[] fileEntries = Directory.GetFiles(applicationPath);
+                foreach (var fileFullPath in fileEntries)
+                {
+                    if (fileFullPath.Contains("CTI.SalesUpload.Console.exe.config"))
+                    {
+                        string text = File.ReadAllText(fileFullPath);
+                        text = text.Replace("{{ClientId}}", txtClientId.Text);
+                        text = text.Replace("{{ClientSecret}}", txtClientSecret.Text);
+                        text = text.Replace("{{IntegrationId}}", txtIntegrationId.Text);
+                        text = text.Replace("{{FilePath}}", txtFilePath.Text);
+                        File.WriteAllText(fileFullPath, text);
+                    }
+                }
+                MessageBox.Show("Installation Complete", ConfigurationManager.AppSettings["Title"], MessageBoxButtons.OK);
+                Application.Exit();
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("An error has occured, please report to system administrator.", ConfigurationManager.AppSettings["Title"], MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
