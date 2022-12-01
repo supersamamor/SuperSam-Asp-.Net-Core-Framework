1a440008987fe7a3b355c3bf70d2a892bc774e49
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-12-01 16:46:17 +0800

    update

---
+++ SuperSam.InstallerCreator/Installer.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace SuperSam.InstallerCreator
+{
+    partial class Installer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Installer
+            // 
+            this.ClientSize = new System.Drawing.Size(506, 259);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Installer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Installer_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SuperSam.InstallerCreator/Installer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperSam.InstallerCreator
+{
+    public partial class Installer : Form
+    {
+        public Installer()
+        {
+            InitializeComponent();
+        }
+
+        private void Installer_Load(object sender, EventArgs e)
+        {
+            this.Text = $"Install {ConfigurationManager.AppSettings["Title"]}";
+        }
+
+        private void Installer_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
