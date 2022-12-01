6841b196c895f2c0d0f58cba661add32f8984251
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-12-01 14:59:29 +0800

    update

--- CTI.SalesUpload.Console/Services/SalesFileUploadService.cs
+++ CTI.SalesUpload.Console/Services/SalesFileUploadService.cs
@@ -19,11 +19,12 @@ namespace CTI.SalesUpload.Console.Services
             {
                 using (var form = new MultipartFormDataContent())
                 {
+
                     _client.DefaultRequestHeaders.Clear();
                     form.Add(content, "FileUpload", Path.GetFileName(filePath));
                     form.Add(new StringContent(ConfigurationManager.AppSettings["IntegrationId"]), "IntegrationId");                  
                     _client.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
-                    using (var response = _client.PostAsync($"/api/TenantPOSSales?api-version={ConfigurationManager.AppSettings["ApiVersion"]}", form, token))
+                    using (var response = _client.PostAsync($"/api/v{ConfigurationManager.AppSettings["ApiVersion"]}/TenantPOSSales/FileUpload", form, token))
                     {
                         response.Result.EnsureSuccessStatusCode();
                     }                        
