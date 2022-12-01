9cda9b7772098ee84639878ed9d8ca0c26e41aad
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-12-01 17:49:13 +0800

    update

--- SuperSam.InstallerCreator/Installer.Designer.cs
+++ SuperSam.InstallerCreator/Installer.Designer.cs
@@ -28,23 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIntegrationId = new System.Windows.Forms.TextBox();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.txtClientId = new System.Windows.Forms.TextBox();
+            this.txtClientSecret = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Client Id :";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Client Secret :";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Integration Id :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "File Path :";
+            // 
+            // txtIntegrationId
+            // 
+            this.txtIntegrationId.Location = new System.Drawing.Point(119, 50);
+            this.txtIntegrationId.Name = "txtIntegrationId";
+            this.txtIntegrationId.Size = new System.Drawing.Size(330, 22);
+            this.txtIntegrationId.TabIndex = 4;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(119, 86);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(330, 22);
+            this.txtFilePath.TabIndex = 5;
+            // 
+            // txtClientId
+            // 
+            this.txtClientId.Location = new System.Drawing.Point(119, 121);
+            this.txtClientId.Name = "txtClientId";
+            this.txtClientId.Size = new System.Drawing.Size(330, 22);
+            this.txtClientId.TabIndex = 6;
+            this.txtClientId.Visible = false;
+            // 
+            // txtClientSecret
+            // 
+            this.txtClientSecret.Location = new System.Drawing.Point(119, 159);
+            this.txtClientSecret.Name = "txtClientSecret";
+            this.txtClientSecret.Size = new System.Drawing.Size(330, 22);
+            this.txtClientSecret.TabIndex = 7;
+            this.txtClientSecret.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Install";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(256, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Installer
             // 
-            this.ClientSize = new System.Drawing.Size(506, 259);
+            this.ClientSize = new System.Drawing.Size(506, 189);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtClientSecret);
+            this.Controls.Add(this.txtClientId);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.txtIntegrationId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Installer";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.Installer_Load_1);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtIntegrationId;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.TextBox txtClientId;
+        private System.Windows.Forms.TextBox txtClientSecret;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- SuperSam.InstallerCreator/Installer.cs
+++ SuperSam.InstallerCreator/Installer.cs
@@ -1,31 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SuperSam.InstallerCreator
 {
     public partial class Installer : Form
     {
+
         public Installer()
         {
             InitializeComponent();
         }
 
-        private void Installer_Load(object sender, EventArgs e)
-        {
-            this.Text = $"Install {ConfigurationManager.AppSettings["Title"]}";
-        }
-
         private void Installer_Load_1(object sender, EventArgs e)
         {
+            string applicationPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            this.Text = $"Install {ConfigurationManager.AppSettings["Title"]}";
+            txtClientId.Text = ConfigurationManager.AppSettings["DefaultClientId"];
+            txtClientSecret.Text = ConfigurationManager.AppSettings["DefaultClientSecret"];
+            txtIntegrationId.Text = ConfigurationManager.AppSettings["DefaultIntegrationId"];
+            txtFilePath.Text = ConfigurationManager.AppSettings["DefaultFilePath"];
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           
         }
     }
 }
