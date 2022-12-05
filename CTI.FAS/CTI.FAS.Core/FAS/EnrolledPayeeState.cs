fef95a81c465849cb5b90cd2cd531ef45ea1451f
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-12-05 14:59:18 +0800

    update

--- CTI.FAS/CTI.FAS.Core/FAS/EnrolledPayeeState.cs
+++ CTI.FAS/CTI.FAS.Core/FAS/EnrolledPayeeState.cs
@@ -4,17 +4,17 @@ namespace CTI.FAS.Core.FAS;
 
 public record EnrolledPayeeState : BaseEntity
 {
-	public string CompanyId { get; init; } = "";
-	public string CreditorId { get; init; } = "";
-	public string PayeeAccountNumber { get; init; } = "";
-	public string PayeeAccountType { get; init; } = "";
-	public string? Status { get; init; } = Constants.EnrollmentStatus.New;
-
-
-	public CompanyState? Company { get; init; }
-	public CreditorState? Creditor { get; init; }
-	
-	public IList<PaymentTransactionState>? PaymentTransactionList { get; set; }
-	public IList<EnrolledPayeeEmailState>? EnrolledPayeeEmailList { get; set; }
-	
+    public string CompanyId { get; init; } = "";
+    public string CreditorId { get; init; } = "";
+    public string PayeeAccountNumber { get; init; } = "";
+    public string PayeeAccountType { get; init; } = "";
+    public string? Status { get; private set; } = Constants.EnrollmentStatus.New;
+    public CompanyState? Company { get; init; }
+    public CreditorState? Creditor { get; init; }
+    public IList<PaymentTransactionState>? PaymentTransactionList { get; set; }
+    public IList<EnrolledPayeeEmailState>? EnrolledPayeeEmailList { get; set; }
+    public void TagAsNew()
+    {
+        this.Status = Constants.EnrollmentStatus.New;
+    }
 }
