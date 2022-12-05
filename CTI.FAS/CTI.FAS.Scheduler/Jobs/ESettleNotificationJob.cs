38cb5cc4ad7990b156df3332a8328ea5c186987e
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-12-05 17:14:27 +0800

    update

--- CTI.FAS/CTI.FAS.Scheduler/Jobs/ESettleNotificationJob.cs
+++ CTI.FAS/CTI.FAS.Scheduler/Jobs/ESettleNotificationJob.cs
@@ -33,8 +33,56 @@ namespace CTI.FAS.Scheduler.Jobs
         }
         private async Task ProcessESettleNotificationAsync()
         {
-           
+            var paymentTransactionToEmailList = await _context.PaymentTransaction
+                .Include(l => l.EnrolledPayee).ThenInclude(l => l!.Creditor)
+                .Include(l => l.EnrolledPayee).ThenInclude(l => l!.Company)
+                .IgnoreQueryFilters().Where(l => l.IsForSending == true).ToListAsync();
+            foreach (var paymentTransactionForEmailSending in paymentTransactionToEmailList)
+            {
+            }
         }
-      
+        private string GenerateEmailTemplate(PaymentTransactionState paymentTransaction)
+        {
+            string str = "<span style='font-size:10pt; font-weight:bold; font-family:Arial;'> ";
+            str = str + "<b>";
+            str = str + paymentTransaction!.EnrolledPayee!.Company!.Name;
+            str = str + "</b>";
+            str = str + "<br />";
+            str = str + "E-Settlement Notification - " + paymentTransaction.DocumentNumber;
+            str = str + "<br />";
+            str = str + "</span>";
+            str = str + "<br />";
+            str = str + "<br />";
+            str = str + "<span style='font-size:10pt; font-family:Arial;'> ";
+            str = str + "Dear Sir/Madam,";
+            str = str + "<br />";
+            str = str + "<br />";
+            str = str + "See Attached for the breakdown of invoices to be paid thru East West Bank E-Settle facility.";
+            str = str + "<br />";
+            str = str + "<br />";
+            str = str + "You will need Adobe Reader in viewing them. Please visit: <a href='http://get.adobe.com/reader/'>http://get.adobe.com/reader/</a> to download the latest version of this software.";
+            str = str + "<br />";
+            str = str + "Adobe Acrobat Reader - &copy; 2013 Adobe Systems Incorporated. All rights reserved.";
+            str = str + "<br />";
+            str = str + "<br />";
+            str = str + "For inquiries, please call FAI Hotline at 8460278 and ask for the local number of the person copied in this email.";
+            str = str + "<br />";
+            str = str + "<br />";
+            str = str + "<i>This is a system-generated email. Please do not reply.<i/>";
+            str = str + "<br />";   
+            str = str + "Copyright &copy; 2015 Filinvest Alabang Inc. All rights reserved.";
+            str = str + "<br /> ";
+            str = str + "<br /> ";
+            str = str + "<hr /> ";
+            str = str + "<span style='font-family:Times New Roman; font-size:8pt'> ";
+            str = str + "<b><p>Email Confidentiality Disclaimer:</b> The information in this electronic message is confidential and/or privileged, and intended for the exclusive use of the ";
+            str = str + "addressee. If you are not the intended recipient, you are notified that disclosure, retention, dissemination, copying, alteration and distribution of this ";
+            str = str + "communication and/or any attachment, or any part thereof or information therein, are strictly prohibited. If you receive this communication and any ";
+            str = str + "attachments in error, kindly notify the sender immediately by email and delete this communication and all attachments. Any views or opinions presented in ";
+            str = str + "this email are solely those of the author and do not necessarily represent those of Filinvest Alabang Inc. </p>";
+            str = str + "</span> ";
+            return str;
+        }
+
     }
 }
