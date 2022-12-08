e306b41f0958afb6aa1a704f979bb0b5b0b413ad
Samuel N. Amor IV <supersam.amor@gmail.com>
2022-12-08 13:37:20 +0800

    upload

--- CTI.FAS/CTI.FAS.CsvGenerator/Services/PaymentTransactionCsvService.cs
+++ CTI.FAS/CTI.FAS.CsvGenerator/Services/PaymentTransactionCsvService.cs
@@ -24,7 +24,43 @@ namespace CTI.FAS.CsvGenerator.Services
             {
                 batchNo = 1;
             }
-            return ESettle(input, entityCode, batchNo, subfolder);
+            if (input?.FirstOrDefault()?.PaymentType == PaymentType.ESettle)
+            {
+                return ESettle(input, entityCode, batchNo, subfolder);
+            }
+            else
+            {
+                return CheckPrepare(input!, entityCode, batchNo, subfolder);
+            }           
+        }
+        private CsvDocumentModel CheckPrepare(IList<PaymentTransactionState> input, string? entityCode, int? batchNo, string subfolder)
+        {
+            var csvDocument = new CsvDocumentModel($"FUNDS_{entityCode}_{DateTime.Now:MMddyyyy}_{batchNo}.csv",
+             _staticFolderPath, subfolder, GlobalConstants.UploadFilesPath);
+            var csv = new StringBuilder();
+            //Append Header 
+            csv.AppendLine(String.Format("{0,380}", "HDR,Participating Account Number,Participating Account Name,Participating Reference Number,Transaction Amount,Transaction Reference Number,Print EWT?,Record Type,ATC Code,Tax Period From,Tax Period To,Amount of Income Payments (1st Quarter),Amount of Income Payments (2nd Quarter),Amount of Income Payment (3rd Quarter),Total Amount of Income Payments,Tax Withheld for the Quarter"));
+            //Append Detail
+            foreach (var item in input)
+            {
+                var supplier = SanitizeSupplierName(item.EnrolledPayee!.Creditor!.PayeeAccountName);
+                var accountNo = SanitizeAccountNo(item.EnrolledPayee!.PayeeAccountNumber);
+                var creditorAccount = SanitizeCreditorCode(item.EnrolledPayee!.Creditor!.CreditorAccount);
+                var newLine = String.Format("{0,3},{1,12},{2," + supplier.Length + "},{3," + accountNo.Length + "},{4," + item.DocumentAmount.ToString().Length + "},{5," + item.DocumentNumber.Length + "},{6,2},{7,0}",
+                                        "DTL", accountNo, supplier,
+                                        creditorAccount, item.DocumentAmount, item.DocumentNumber, "No", ",,,,,,,,,");
+                csv.AppendLine(newLine);
+            }
+            //Append Summary
+            var totalAmount = input.Sum(l => l.DocumentAmount);
+            csv.AppendLine(String.Format("{0,3},{1," + input.Count.ToString().Length + "},{2," + totalAmount.ToString().Length + "},{3,0}",
+                             "TLR",
+                             input.Count,
+                             totalAmount,
+                             ",,,,,,,,,,,,,"));
+
+            File.WriteAllText(csvDocument.CompleteFilePath, csv.ToString());
+            return csvDocument;
         }
         private CsvDocumentModel ESettle(IList<PaymentTransactionState> input, string? entityCode, int? batchNo, string subfolder)
         {
