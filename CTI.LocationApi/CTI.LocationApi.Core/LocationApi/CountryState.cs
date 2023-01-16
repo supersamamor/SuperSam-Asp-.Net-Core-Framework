426a133464219771e8cc3ad7b8994d2645e2b61d
Samuel N. Amor IV <supersam.amor@gmail.com>
2023-01-16 14:32:24 +0800

    update

--- CTI.LocationApi/CTI.LocationApi.Core/LocationApi/CountryState.cs
+++ CTI.LocationApi/CTI.LocationApi.Core/LocationApi/CountryState.cs
@@ -7,8 +7,9 @@ public record CountryState : BaseEntity
 {
 	public string Name { get; init; } = "";
 	public string Code { get; init; } = "";
-	
-	
+	public string Citizenship { get; set; } = "";
+	public string AreaCode { get; set; } = "";
+
 	public IList<RegionState>? RegionList { get; set; }
 	
 }
