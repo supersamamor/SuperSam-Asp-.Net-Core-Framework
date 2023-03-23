90ac8175eeaae2f0c6c32df79c104796bbd9e7a9
Samuel N. Amor IV <supersam.amor@gmail.com>
2023-03-23 14:53:09 +0800

    update all

---
+++ CTI.ServiceAPI/src/Application/MainModulePlaceHolder/Queries/GetMainModulePlaceHoldersByPrimaryKey/GetMainModulePlaceHolderByPrimaryKeyQuery.cs
@@ -0,0 +1,39 @@
+using Ardalis.Specification;
+using ProjectNamePlaceHolder.Services.Application.MainModulePlaceHolder.DTOs;
+using Cti.Core.Application.Common.Persistence;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjectNamePlaceHolder.Services.Application.MainModulePlaceHolder.Queries.GetMainModulePlaceHoldersByPrimaryKey
+{
+    public class GetMainModulePlaceHolderByPrimaryKeyQuery : IRequest<MainModulePlaceHolderDto>
+    {
+        public string PrimaryKey { get; set; } = "";
+    }
+
+    public class GetMainModulePlaceHoldersByProjectQueryHandler : IRequestHandler<GetMainModulePlaceHolderByPrimaryKeyQuery, MainModulePlaceHolderDto>
+    {
+        private readonly IRepository<Domain.Entities.MainModulePlaceHolder> _mainModulePlaceHolderRepository;
+
+        public GetMainModulePlaceHoldersByProjectQueryHandler(IRepository<Domain.Entities.MainModulePlaceHolder> mainModulePlaceHolderRepository)
+        {
+            _mainModulePlaceHolderRepository = mainModulePlaceHolderRepository;
+        }
+
+        public async Task<MainModulePlaceHolderDto> Handle(GetMainModulePlaceHolderByPrimaryKeyQuery request, CancellationToken cancellationToken)
+        {
+            var spec = new MainModulePlaceHolderByPrimaryKeyWithDetailsSpec(request.PrimaryKey);
+            var unitsDto = await _mainModulePlaceHolderRepository.SingleOrDefaultAsync(spec);
+            return unitsDto;
+        }
+    }
+
+    public class MainModulePlaceHolderByPrimaryKeyWithDetailsSpec : SingleResultSpecification<Domain.Entities.MainModulePlaceHolder, MainModulePlaceHolderDto>
+    {
+        public MainModulePlaceHolderByPrimaryKeyWithDetailsSpec(string primaryKey)
+        {
+            Query.Where(x => x.PrimaryKey == primaryKey);
+        }
+    }
+}
--- CTI.ServiceAPI/src/Service.API/Controllers/MainModulePlaceHolderController.cs
+++ CTI.ServiceAPI/src/Service.API/Controllers/MainModulePlaceHolderController.cs
@@ -1,9 +1,9 @@
-using ProjectNamePlaceHolder.Services.Application.MainModulePlaceHolders.DTOs;
+using ProjectNamePlaceHolder.Services.Application.MainModulePlaceHolder.DTOs;
 using Cti.Core.Application.Common.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using ProjectNamePlaceHolder.Services.Application.MainModulePlaceHolders.Queries.GetMainModulePlaceHoldersByPrimaryKey;
-using ProjectNamePlaceHolder.Services.Application.MainModulePlaceHolders.Queries.GetMainModulePlaceHoldersPaged;
+using ProjectNamePlaceHolder.Services.Application.MainModulePlaceHolder.Queries.GetMainModulePlaceHoldersByPrimaryKey;
+using ProjectNamePlaceHolder.Services.Application.MainModulePlaceHolder.Queries.GetMainModulePlaceHoldersPaged;
 
 namespace ProjectNamePlaceHolder.Services.API.Controllers
 {
@@ -12,7 +12,7 @@ namespace ProjectNamePlaceHolder.Services.API.Controllers
         [HttpGet("{primaryKey}")]
         public async Task<MainModulePlaceHolderDto> GetAsync(string primaryKey)
         {
-            var results = await Mediator.Send(new GetMainModulePlaceHoldersByPrimaryKeyQuery() { PrimaryKey = primaryKey });
+            var results = await Mediator.Send(new GetMainModulePlaceHolderByPrimaryKeyQuery() { PrimaryKey = primaryKey });
             return results;
         }
 
