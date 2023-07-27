1bb94cb03ef41aec9ec6d97fd0fe9fc44dd41948
CORP\cti00026 <sam.amor@corptech.it>
2023-07-27 10:08:11 +0800

    update

--- CTI.DPI/CTI.DPI.Core/Constants/ReportChartType.cs
+++ CTI.DPI/CTI.DPI.Core/Constants/ReportChartType.cs
@@ -4,6 +4,13 @@ namespace CTI.DPI.Core.Constants
     {
         public const string Table = "Table";
         public const string HorizontalBar = "Horizontal Bar";
-        public const string Pie = "Pie";      
+        public const string Pie = "Pie";
+
+        public static readonly Dictionary<string, string> ChartToolTip = new()
+        {
+            { "Table", "" },
+            { "Horizontal Bar", "Your query should consist of label and data fields (eg. Select 'Record 1' as <b>Label</b>, 10 as  <b>Data</b>)" },
+            { "Pie", "Your query should consist of label and data fields (eg. Select 'Record 1' as <b>Label</b>, 10 as  <b>Data</b>)" },
+        };
     }
 }
