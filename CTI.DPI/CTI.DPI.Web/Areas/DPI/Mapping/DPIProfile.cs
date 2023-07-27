1ae707c80f50c258120c841f88567feae391961f
CORP\cti00026 <sam.amor@corptech.it>
2023-07-27 15:53:54 +0800

    update

--- CTI.DPI/CTI.DPI.Web/Areas/DPI/Mapping/DPIProfile.cs
+++ CTI.DPI/CTI.DPI.Web/Areas/DPI/Mapping/DPIProfile.cs
@@ -3,13 +3,6 @@ using CTI.DPI.Core.DPI;
 using CTI.DPI.Web.Areas.DPI.Models;
 using CTI.DPI.Application.Features.DPI.Approval.Commands;
 using CTI.DPI.Application.Features.DPI.Report.Commands;
-using CTI.DPI.Application.Features.DPI.ReportTable.Commands;
-using CTI.DPI.Application.Features.DPI.ReportTableJoinParameter.Commands;
-using CTI.DPI.Application.Features.DPI.ReportColumnHeader.Commands;
-using CTI.DPI.Application.Features.DPI.ReportColumnDetail.Commands;
-using CTI.DPI.Application.Features.DPI.ReportFilterGrouping.Commands;
-using CTI.DPI.Application.Features.DPI.ReportColumnFilter.Commands;
-using CTI.DPI.Application.Features.DPI.ReportQueryFilter.Commands;
 using CTI.DPI.Application.DTOs;
 
 namespace CTI.DPI.Web.Areas.DPI.Mapping;
@@ -21,32 +14,25 @@ public class DPIProfile : Profile
         CreateMap<ReportViewModel, AddReportCommand>();
 		CreateMap<ReportViewModel, EditReportCommand>();
 		CreateMap<ReportState, ReportViewModel>().ReverseMap();
-		CreateMap<ReportTableViewModel, AddReportTableCommand>();
-		CreateMap<ReportTableViewModel, EditReportTableCommand>();
+	
 		CreateMap<ReportTableState, ReportTableViewModel>().ForPath(e => e.ForeignKeyReport, o => o.MapFrom(s => s.Report!.ReportName));
 		CreateMap<ReportTableViewModel, ReportTableState>();
-		CreateMap<ReportTableJoinParameterViewModel, AddReportTableJoinParameterCommand>();
-		CreateMap<ReportTableJoinParameterViewModel, EditReportTableJoinParameterCommand>();
+	
 		CreateMap<ReportTableJoinParameterState, ReportTableJoinParameterViewModel>().ForPath(e => e.ForeignKeyReportTable, o => o.MapFrom(s => s.ReportTable!.Id)).ForPath(e => e.ForeignKeyReport, o => o.MapFrom(s => s.Report!.ReportName));
 		CreateMap<ReportTableJoinParameterViewModel, ReportTableJoinParameterState>();
-		CreateMap<ReportColumnHeaderViewModel, AddReportColumnHeaderCommand>();
-		CreateMap<ReportColumnHeaderViewModel, EditReportColumnHeaderCommand>();
+
 		CreateMap<ReportColumnHeaderState, ReportColumnHeaderViewModel>().ForPath(e => e.ForeignKeyReport, o => o.MapFrom(s => s.Report!.ReportName));
 		CreateMap<ReportColumnHeaderViewModel, ReportColumnHeaderState>();
-		CreateMap<ReportColumnDetailViewModel, AddReportColumnDetailCommand>();
-		CreateMap<ReportColumnDetailViewModel, EditReportColumnDetailCommand>();
+
 		CreateMap<ReportColumnDetailState, ReportColumnDetailViewModel>().ForPath(e => e.ForeignKeyReportTable, o => o.MapFrom(s => s.ReportTable!.Id)).ForPath(e => e.ForeignKeyReportColumnHeader, o => o.MapFrom(s => s.ReportColumnHeader!.Id));
 		CreateMap<ReportColumnDetailViewModel, ReportColumnDetailState>();
-		CreateMap<ReportFilterGroupingViewModel, AddReportFilterGroupingCommand>();
-		CreateMap<ReportFilterGroupingViewModel, EditReportFilterGroupingCommand>();
+	
 		CreateMap<ReportFilterGroupingState, ReportFilterGroupingViewModel>().ForPath(e => e.ForeignKeyReport, o => o.MapFrom(s => s.Report!.ReportName));
 		CreateMap<ReportFilterGroupingViewModel, ReportFilterGroupingState>();
-		CreateMap<ReportColumnFilterViewModel, AddReportColumnFilterCommand>();
-		CreateMap<ReportColumnFilterViewModel, EditReportColumnFilterCommand>();
+
 		CreateMap<ReportColumnFilterState, ReportColumnFilterViewModel>().ForPath(e => e.ForeignKeyReportTable, o => o.MapFrom(s => s.ReportTable!.Id)).ForPath(e => e.ForeignKeyReportFilterGrouping, o => o.MapFrom(s => s.ReportFilterGrouping!.Id));
 		CreateMap<ReportColumnFilterViewModel, ReportColumnFilterState>();
-		CreateMap<ReportQueryFilterViewModel, AddReportQueryFilterCommand>();
-		CreateMap<ReportQueryFilterViewModel, EditReportQueryFilterCommand>();
+	
 		CreateMap<ReportQueryFilterState, ReportQueryFilterViewModel>().ForPath(e => e.ForeignKeyReport, o => o.MapFrom(s => s.Report!.ReportName));
 		CreateMap<ReportQueryFilterViewModel, ReportQueryFilterState>();
 		
