a6531976566caac22a4f2876490078b13bdcb5d6
CORP\cti00026 <sam.amor@corptech.it>
2023-09-15 11:48:03 +0800

    update to .net core 7

--- CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Infrastructure/Migrations/Identity/IdentityContextModelSnapshot.cs
+++ CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Infrastructure/Migrations/Identity/IdentityContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace CompanyNamePlaceHolder.ProjectNamePlaceHolder.Infrastructure.Migration
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.19")
+                .HasAnnotation("ProductVersion", "7.0.11")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
