8d2d3770fbf789666c047e26d804bc1461506234
CORP\cti00026 <sam.amor@corptech.it>
2023-09-19 10:07:34 +0800

    update

--- GraphAndReportPatch/Resources/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/Service/DropdownServices.cs
+++ GraphAndReportPatch/Resources/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web/Service/DropdownServices.cs
@@ -20,14 +20,6 @@ namespace CompanyNamePlaceHolder.ProjectNamePlaceHolder.Web.Service
 			_context = context;
 			_mediaTr = mediaTr;
 		}
-
-        public SelectList GetReportTableList(string? id)
-        {
-            return _context.GetSingle<ReportTableState>(e => e.Id == id, new()).Result.Match(
-                Some: e => new SelectList(new List<SelectListItem> { new() { Value = e.Id, Text = e.Id } }, "Value", "Text", e.Id),
-                None: () => new SelectList(new List<SelectListItem>(), "Value", "Text")
-            );
-        }
         public async Task<IEnumerable<SelectListItem>> GetRoleList()
         {
             return (await _mediaTr.Send(new GetRolesQuery())).Data.Select(l => new SelectListItem { Value = l.Name, Text = l.Name });
