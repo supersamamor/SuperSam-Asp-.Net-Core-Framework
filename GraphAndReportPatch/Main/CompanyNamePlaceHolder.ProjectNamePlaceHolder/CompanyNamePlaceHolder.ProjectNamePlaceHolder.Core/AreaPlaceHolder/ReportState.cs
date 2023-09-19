8cdc3dfb8ab368d8fc4ce4f3f9ca3faa05e88b90
CORP\cti00026 <sam.amor@corptech.it>
2023-09-19 09:39:18 +0800

    update

--- GraphAndReportPatch/Main/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Core/AreaPlaceHolder/ReportState.cs
+++ GraphAndReportPatch/Main/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Core/AreaPlaceHolder/ReportState.cs
@@ -9,85 +9,10 @@ public record ReportState : BaseEntity
 	public string? QueryString { get; init; }
     public bool DisplayOnDashboard { get; init; }
 	public bool DisplayOnReportModule { get; init; }
-    public int Sequence { get; init; }
-    public IList<ReportTableState>? ReportTableList { get; set; }
-	public IList<ReportTableJoinParameterState>? ReportTableJoinParameterList { get; set; }
-	public IList<ReportColumnHeaderState>? ReportColumnHeaderList { get; set; }
-	public IList<ReportFilterGroupingState>? ReportFilterGroupingList { get; set; }
+    public int Sequence { get; init; }   
 	public IList<ReportQueryFilterState>? ReportQueryFilterList { get; set; }
     public IList<ReportRoleAssignmentState>? ReportRoleAssignmentList { get; set; }
 }
-
-public record ReportTableState : BaseEntity
-{
-    public string ReportId { get; init; } = "";
-    public string TableName { get; init; } = "";
-    public string? Alias { get; init; }
-    public string? JoinType { get; init; }
-    public int Sequence { get; init; }
-    public ReportState? Report { get; init; }
-    public IList<ReportTableJoinParameterState>? ReportTableJoinParameterList { get; set; }
-    public IList<ReportColumnDetailState>? ReportColumnDetailList { get; set; }
-    public IList<ReportColumnFilterState>? ReportColumnFilterList { get; set; }
-
-}
-public record ReportTableJoinParameterState : BaseEntity
-{
-    public string ReportId { get; init; } = "";
-    public string? LogicalOperator { get; init; }
-    public string TableId { get; init; } = "";
-    public string FieldName { get; init; } = "";
-    public string JoinFromTableId { get; init; } = "";
-    public string JoinFromFieldName { get; init; } = "";
-    public int Sequence { get; init; }
-    public ReportState? Report { get; init; }
-    public ReportTableState? ReportTable { get; init; }
-}
-
-public record ReportColumnHeaderState : BaseEntity
-{
-    public string? ReportId { get; init; }
-    public string? Alias { get; init; }
-    public string? AggregationOperator { get; init; }
-    public ReportState? Report { get; init; }
-    public IList<ReportColumnDetailState>? ReportColumnDetailList { get; set; }
-
-}
-public record ReportColumnDetailState : BaseEntity
-{
-    public string? ReportColumnId { get; init; }
-    public string? TableId { get; init; }
-    public string? FieldName { get; init; }
-    public string? Function { get; init; }
-    public string? ArithmeticOperator { get; init; }
-    public int? Sequence { get; init; }
-    public ReportColumnHeaderState? ReportColumnHeader { get; init; }
-    public ReportTableState? ReportTable { get; init; }
-}
-
-public record ReportColumnFilterState : BaseEntity
-{
-    public string? ReportFilterGroupingId { get; init; }
-    public string? LogicalOperator { get; init; }
-    public string? TableId { get; init; }
-    public string? FieldName { get; init; }
-    public string? ComparisonOperator { get; init; }
-    public bool IsString { get; init; }
-    public ReportFilterGroupingState? ReportFilterGrouping { get; init; }
-    public ReportTableState? ReportTable { get; init; }
-
-}
-
-public record ReportFilterGroupingState : BaseEntity
-{
-    public string? ReportId { get; init; }
-    public string? LogicalOperator { get; init; }
-    public int? GroupLevel { get; init; }
-    public int? Sequence { get; init; }
-    public ReportState? Report { get; init; }
-    public IList<ReportColumnFilterState>? ReportColumnFilterList { get; set; }
-}
-
 public record ReportQueryFilterState : BaseEntity
 {
     public string ReportId { get; init; } = "";
