3475b0598bade1b585aa38e65590f804f51862cf
CORP\cti00026 <sam.amor@corptech.it>
2023-10-25 17:46:18 +0800

    async

--- CTI-Core7/Main/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.Common.Data/AuditableContext.cs
+++ CTI-Core7/Main/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.Common.Data/AuditableContext.cs
@@ -28,74 +28,74 @@ public abstract class AuditableContext : DbContext
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
     public virtual async Task<int> SaveChangesAsync(string? userId = null, string? traceId = null, CancellationToken cancellationToken = default)
-    {
-        var auditEntries = OnBeforeSaveChanges(userId, traceId);
-        var result = await base.SaveChangesAsync(cancellationToken);
-        await OnAfterSaveChanges(auditEntries);
-        return result;
-    }
+	{
+		var auditEntries = await OnBeforeSaveChanges(userId, traceId);
+		var result = await base.SaveChangesAsync(cancellationToken);
+		await OnAfterSaveChanges(auditEntries);
+		return result;
+	}
 
-    private List<AuditEntry> OnBeforeSaveChanges(string? userId, string? traceId)
-    {
-        ChangeTracker.DetectChanges();
-        var auditEntries = new List<AuditEntry>();
-        foreach (var entry in ChangeTracker.Entries())
-        {
-            if (entry.Entity is Audit || entry.State == EntityState.Detached || entry.State == EntityState.Unchanged)
-                continue;
+	private async Task<List<AuditEntry>> OnBeforeSaveChanges(string? userId, string? traceId)
+	{
+		ChangeTracker.DetectChanges();
+		var auditEntries = new List<AuditEntry>();
 
-            var auditEntry = new AuditEntry(entry)
-            {
-                TableName = entry.Entity.GetType().Name,
-                UserId = userId,
-                TraceId = traceId
-            };
-            auditEntries.Add(auditEntry);
-            foreach (var property in entry.Properties)
-            {
-                if (property.IsTemporary)
-                {
-                    auditEntry.TemporaryProperties.Add(property);
-                    continue;
-                }
+		foreach (var entry in ChangeTracker.Entries())
+		{
+			if (entry.Entity is Audit || entry.State == EntityState.Detached || entry.State == EntityState.Unchanged)
+				continue;
 
-                var propertyName = property.Metadata.Name;
-                if (property.Metadata.IsPrimaryKey())
-                {
-                    auditEntry.KeyValues[propertyName] = property.CurrentValue;
-                    continue;
-                }
+			var auditEntry = new AuditEntry(entry)
+			{
+				TableName = entry.Entity.GetType().Name,
+				UserId = userId,
+				TraceId = traceId
+			};
+			auditEntries.Add(auditEntry);
 
-                switch (entry.State)
-                {
-                    case EntityState.Added:
-                        auditEntry.AuditType = AuditType.Create;
-                        auditEntry.NewValues[propertyName] = property.CurrentValue;
-                        break;
+			foreach (var property in entry.Properties)
+			{
+				if (property.IsTemporary)
+				{
+					auditEntry.TemporaryProperties.Add(property);
+					continue;
+				}
 
-                    case EntityState.Deleted:
-                        auditEntry.AuditType = AuditType.Delete;
-                        auditEntry.OldValues[propertyName] = property.OriginalValue;
-                        break;
+				var propertyName = property.Metadata.Name;
+				if (property.Metadata.IsPrimaryKey())
+				{
+					auditEntry.KeyValues[propertyName] = property.CurrentValue;
+					continue;
+				}
 
-                    case EntityState.Modified:
-                        if (property.IsModified)
-                        {
-                            auditEntry.ChangedColumns.Add(propertyName);
-                            auditEntry.AuditType = AuditType.Update;
-                            auditEntry.OldValues[propertyName] = property.OriginalValue;
-                            auditEntry.NewValues[propertyName] = property.CurrentValue;
-                        }
-                        break;
-                }
-            }
-        }
-        foreach (var auditEntry in auditEntries.Where(_ => !_.HasTemporaryProperties))
-        {
-            AuditLogs.Add(auditEntry.ToAudit());
-        }
-        return auditEntries.Where(_ => _.HasTemporaryProperties).ToList();
-    }
+				switch (entry.State)
+				{
+					case EntityState.Added:
+						auditEntry.AuditType = AuditType.Create;
+						auditEntry.NewValues[propertyName] = property.CurrentValue;
+						break;
+
+					case EntityState.Deleted:
+						auditEntry.AuditType = AuditType.Delete;
+						auditEntry.OldValues[propertyName] = property.OriginalValue;
+						break;
+
+					case EntityState.Modified:
+						if (property.IsModified)
+						{
+							auditEntry.ChangedColumns.Add(propertyName);
+							auditEntry.AuditType = AuditType.Update;
+							auditEntry.OldValues[propertyName] = property.OriginalValue;
+							auditEntry.NewValues[propertyName] = property.CurrentValue;
+						}
+						break;
+				}
+			}
+		}
+		// Use asynchronous AddRange to add audit entries to the context
+		await AuditLogs.AddRangeAsync(auditEntries.Where(_ => !_.HasTemporaryProperties).Select(auditEntry => auditEntry.ToAudit()));
+		return auditEntries.Where(_ => _.HasTemporaryProperties).ToList();
+	}
 
     private Task OnAfterSaveChanges(List<AuditEntry> auditEntries)
     {
