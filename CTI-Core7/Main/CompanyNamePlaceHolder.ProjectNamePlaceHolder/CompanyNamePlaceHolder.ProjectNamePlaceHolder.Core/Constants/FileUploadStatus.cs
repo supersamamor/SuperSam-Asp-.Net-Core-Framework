a898e0a1e97b651e0486aea294a5a361527733aa
CORP\cti00026 <sam.amor@corptech.it>
2023-11-14 16:04:12 +0800

    Upload New Files

---
+++ CTI-Core7/Main/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Core/Constants/FileUploadStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace CompanyNamePlaceHolder.ProjectNamePlaceHolder.Core.Constants
+{
+    public static class FileUploadStatus
+    {
+        public const string Pending = "Pending";
+        public const string Failed = "Failed";
+        public const string Done = "Done";
+    }
+}
