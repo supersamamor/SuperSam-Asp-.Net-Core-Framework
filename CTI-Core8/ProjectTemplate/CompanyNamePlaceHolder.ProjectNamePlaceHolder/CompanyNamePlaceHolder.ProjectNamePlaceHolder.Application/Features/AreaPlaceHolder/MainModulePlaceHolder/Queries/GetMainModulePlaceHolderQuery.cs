974d748763831550088d4f02bf3f911cdcf982d3
CORP\cti00026 <sam.amor@corptech.it>
2024-02-02 15:21:30 +0800

    Update

--- CTI-Core8/ProjectTemplate/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Application/Features/AreaPlaceHolder/MainModulePlaceHolder/Queries/GetMainModulePlaceHolderQuery.cs
+++ CTI-Core8/ProjectTemplate/CompanyNamePlaceHolder.ProjectNamePlaceHolder/CompanyNamePlaceHolder.ProjectNamePlaceHolder.Application/Features/AreaPlaceHolder/MainModulePlaceHolder/Queries/GetMainModulePlaceHolderQuery.cs
@@ -5,15 +5,14 @@ using CompanyNamePlaceHolder.ProjectNamePlaceHolder.Infrastructure.Data;
 using MediatR;
 using CompanyNamePlaceHolder.Common.Utility.Extensions;
 using Microsoft.EntityFrameworkCore;
+using CompanyNamePlaceHolder.ProjectNamePlaceHolder.Application.DTOs;
+using LanguageExt;
 
 namespace CompanyNamePlaceHolder.ProjectNamePlaceHolder.Application.Features.AreaPlaceHolder.MainModulePlaceHolder.Queries;
 
-public record GetMainModulePlaceHolderQuery : BaseQuery, IRequest<PagedListResponse<MainModulePlaceHolderState>>;
+public record GetMainModulePlaceHolderQuery : BaseQuery, IRequest<PagedListResponse<MainModulePlaceHolderListDto>>;
 
-public class GetMainModulePlaceHolderQueryHandler : BaseQueryHandler<ApplicationContext, MainModulePlaceHolderState, GetMainModulePlaceHolderQuery>, IRequestHandler<GetMainModulePlaceHolderQuery, PagedListResponse<MainModulePlaceHolderState>>
+public class GetMainModulePlaceHolderQueryHandler(ApplicationContext context) : BaseQueryHandler<ApplicationContext, MainModulePlaceHolderListDto, GetMainModulePlaceHolderQuery>, IRequestHandler<GetMainModulePlaceHolderQuery, PagedListResponse<MainModulePlaceHolderListDto>>
 {
-    public GetMainModulePlaceHolderQueryHandler(ApplicationContext context) : base(context)
-    {
-    }
 	Template:[ListingQueryIncludeParent]
 }
